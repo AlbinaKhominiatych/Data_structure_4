8c9a30d4e6e0ee48a66d9b1b17dbb6b7a3023d51
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2023-12-06 21:05:22 +0200

    реалізовано додавання слів

--- main.py
+++ main.py
@@ -1,87 +1,33 @@
-#код бінарного дерева
-
-class Node:
-    def __init__(self, value):
-        self.value = value
+class TreeNode:
+    def __init__(self, key, translation):
+        self.key = key
+        self.translation = translation
         self.left = None
         self.right = None
+        self.counter = 1
 
-
-class BinaryTree:
+class DictionaryTree:
     def __init__(self):
         self.root = None
-        self.count = 0
+        self.popular = []
 
-    def insert(self, value):
-        self.count += 1
-
-        if self.root is None:
-            self.root = Node(value)
+    def add_word(self, key, translation):
+        if not self.root:
+            self.root = TreeNode(key, translation)
         else:
-            self._insert_recursive(value, self.root)
+            self._add_word(self.root, key, translation)
 
-
-    def _insert_recursive(self, value, current_node):
-        if value < current_node.value:
-            if current_node.left is None:
-                current_node.left = Node(value)
+    def _add_word(self, node, key, translation):
+        if key == node.key:
+            node.translation = translation
+            node.counter += 1 #звернути увагу на підрахунок
+        elif key < node.key:
+            if node.left is None:
+                node.left = TreeNode(key, translation)
             else:
-                self._insert_recursive(value, current_node.left)
-
-        elif value > current_node.value:
-            if current_node.right is None:
-                current_node.right = Node(value)
-            else:
-                self._insert_recursive(value, current_node.right)
-
-    def inorder_travelsal(self):
-        self._inorder_travelsal_recursive(self.root)
-
-    def _inorder_travelsal_recursive(self, current_node):
-        if current_node is not None:
-            self._inorder_travelsal_recursive(current_node.left)
-            print(current_node.value)
-            self._inorder_travelsal_recursive(current_node.right)
-
-    def search(self, value):
-        return self._search_recursive(value, self.root)
-
-    def _search_recursive(self, value, current_node):
-        if current_node is None:
-            return False
-        elif current_node.value == value:
-            return True
-        elif value < current_node.value:
-            return self._search_recursive(value, current_node.left)
+                self._add_word(node.left, key, translation)
         else:
-            return self._search_recursive(value, current_node.right)
-
-    def find_min(self):
-        current = self.root
-        while current.left is not None:
-            current = current.left
-        return current.value
-
-    def find_max(self):
-        current = self.root
-        while current.right is not None:
-            current = current.right
-        return current.value
-import random
-tree = BinaryTree()
-lst_rand = random.sample(range(10, 99), 8)
-for i in range(8):
-    tree.insert(lst_rand[i])
-tree.inorder_travelsal()
-
-from binarytree import build
-# Список вузлів
-# Побудова бінарного дерева
-binary_tree = build(lst_rand)
-print('Бінарне дерево зі списку :\n',
-	binary_tree)
-
-# Отримання списку вузлів з
-# бінарного дерева
-print('\nСписок із бінарного дерева :',
-	binary_tree.values)
+            if node.right is None:
+                node.right = TreeNode(key, translation)
+            else:
+                self._add_word(node.right, key, translation)

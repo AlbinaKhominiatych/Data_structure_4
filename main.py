7489ca760e62885cd02f480a94fd7668fcdd9ea0
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2023-12-06 21:19:09 +0200

    переписати популярні, працює по додаванню перекладу популярних слів

--- main.py
+++ main.py
@@ -31,3 +31,29 @@ class DictionaryTree:
                 node.right = TreeNode(key, translation)
             else:
                 self._add_word(node.right, key, translation)
+
+    def display_top_popular(self):
+        self.popular = []
+        self._get_popular_words(self.root)
+        if self.popular:
+            print("Топ 10")
+            for word in self.popular[:10]:
+                print(f'{word.key}: {word.counter} було додано разів')
+            else:
+                print("Слова відсутні")
+
+
+    def _get_popular_words(self, node):
+        if node is not None:
+            self._get_popular_words(node.left)
+            self.popular.append(node)
+            self.popular.sort(key=lambda x: x.counter, reverse=True)
+            if len(self.popular)>10:
+                self.popular.pop()
+            self._get_popular_words(node.right)
+
+dictionary = DictionaryTree()
+dictionary.add_word("apple", "яблуко")
+dictionary.add_word("apple", "яблучко")
+dictionary.add_word("banana", "банан")
+dictionary.display_top_popular()
